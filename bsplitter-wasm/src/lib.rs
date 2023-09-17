65dadb5368ab195ede147f6eaa3f1c5896dd3b62
Ronnie Curtis <ronnie.curtis@franklin.ai>
2023-09-17 15:48:22 +1000

    wip: brighten image in place

--- bsplitter-wasm/src/lib.rs
+++ bsplitter-wasm/src/lib.rs
@@ -1,4 +1,6 @@
 mod utils;
+use image::RgbaImage;
+use image::imageops::colorops::brighten_in_place;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::Clamped;
 use web_sys::{console::log_1, CanvasRenderingContext2d, ImageData};
@@ -20,6 +22,8 @@ pub fn greet() {
 
 #[wasm_bindgen]
 pub fn process_image(ctx: &CanvasRenderingContext2d, source_image_data: &mut [u8], width: u32, height: u32) -> Result<(), JsValue> {
-    let new_image_data = ImageData::new_with_u8_clamped_array_and_sh(Clamped(source_image_data), width, height)?;
+    let mut image_buffer = RgbaImage::from_raw(width, height, source_image_data.to_vec()).unwrap_throw();
+    brighten_in_place(&mut image_buffer, 50);
+    let new_image_data = ImageData::new_with_u8_clamped_array_and_sh(Clamped(&image_buffer), width, height)?;
     ctx.put_image_data(&new_image_data, 0.0, 0.0)
 }
